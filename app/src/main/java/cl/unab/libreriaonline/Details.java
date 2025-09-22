723e14af264146a837e98a28149da46933fb3546
Javier Rojas <rojas.javier.dev@gmail.com>
2025-09-22 20:51:50 -0300

    Se realiza el puente para el traspaso de informacion del ListFragment al DetailsFragment

--- app/src/main/java/cl/unab/libreriaonline/Details.java
+++ app/src/main/java/cl/unab/libreriaonline/Details.java
@@ -7,8 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Details extends Fragment {
+
+    private ImageView img;
+    private TextView title, description;
 
 
     public Details() {
@@ -24,6 +30,17 @@ public class Details extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+
+        img = view.findViewById(R.id.image);
+        title = view.findViewById(R.id.title);
+        description = view.findViewById(R.id.description);
+
+        DetailsArgs args = DetailsArgs.fromBundle(getArguments());
+        title.setText(args.getTitle());
+        description.setText(args.getDescription());
+        img.setImageResource(args.getImg());
+
+        return view;
     }
 }
